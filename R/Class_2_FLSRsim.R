59367bd949a0a2e4dd77017d5379e604cd299c2e
klsys <43671812+klsys@users.noreply.github.com>
2022-12-05 15:47:42 +0100

    Update Class_2_FLSRsim.R

--- R/Class_2_FLSRsim.R
+++ R/Class_2_FLSRsim.R
@@ -163,6 +163,7 @@ FLSRsim <- function(...){
         
     if('model' %in% slots){
         # x <- list("model" = c("ricker","bevholt","segreg"))
+	 nmparams1 <-  nmparams2 <- NULL     
         if(!all(grepl('~', x[['model']]))) nmparams1 <- unique(unlist(lapply(x[['model']][!grepl('~', x[['model']])], function(m)all.vars(get(m)()[[2]]))))
 	if(any(grepl('~', x[['model']]))) nmparams2 <- unique(unlist(lapply(x[['model']][grepl('~', x[['model']])],function(x)all.vars(formula(x)))[[3]]))
         # nmparams <- all.vars(get(x[['model']], pos = 2)()[[2]])
